0cecf952d56dfc8bc9fee14ff5426e03fbbf0772
Vasudev <vasudev7412@gmail.com>
2020-05-02 17:03:02 +0200

    Added basic bottom navigation with four tabs

--- app/src/main/java/com/teamrocket/app/ui/main/MainActivity.java
+++ app/src/main/java/com/teamrocket/app/ui/main/MainActivity.java
@@ -1,8 +1,10 @@
-package com.teamrocket.app;
+package com.teamrocket.app.ui.main;
+
+import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.teamrocket.app.R;
 
 public class MainActivity extends AppCompatActivity {
 
