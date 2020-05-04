86f1c7fc4bfa2c6dd4c471589e7b38e329d833dd
Vasudev <vasudev7412@gmail.com>
2020-05-04 17:10:28 +0200

    Show fragments when buttons in bottom nav bar are clicked

--- app/src/main/java/com/teamrocket/app/ui/main/MainActivity.java
+++ app/src/main/java/com/teamrocket/app/ui/main/MainActivity.java
@@ -3,14 +3,57 @@ package com.teamrocket.app.ui.main;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.teamrocket.app.R;
+import com.teamrocket.app.ui.home.HomeFragment;
+import com.teamrocket.app.ui.map.MapFragment;
+import com.teamrocket.app.ui.search.SearchFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private HomeFragment homeFragment;
+    private MapFragment mapFragment;
+    private SearchFragment searchFragment;
+
+    private String currentFragmentTag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        homeFragment = new HomeFragment();
+        mapFragment = new MapFragment();
+        searchFragment = new SearchFragment();
+
+        BottomNavigationView bottomNavBar = findViewById(R.id.bottomNavBar);
+        bottomNavBar.setOnNavigationItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.main_nav_home) showFragment(HomeFragment.TAG);
+            else if (itemId == R.id.main_nav_map) showFragment(MapFragment.TAG);
+            else showFragment(SearchFragment.TAG);
+
+            return true;
+        });
+
+        showFragment(HomeFragment.TAG);
+    }
+
+    private void showFragment(String tag) {
+        if (tag.equals(currentFragmentTag)) {
+            return;
+        }
+
+        currentFragmentTag = tag;
+        Fragment newFragment = currentFragmentTag.equals(HomeFragment.TAG) ? homeFragment
+                : currentFragmentTag.equals(MapFragment.TAG) ? mapFragment
+                : searchFragment;
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.mainContent, newFragment, currentFragmentTag)
+                .commit();
     }
 }
---
+++ app/src/main/java/com/teamrocket/app/ui/search/SearchFragment.java
@@ -0,0 +1,23 @@
+package com.teamrocket.app.ui.search;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.teamrocket.app.R;
+
+public class SearchFragment extends Fragment {
+
+    public static final String TAG = "searchFragment";
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+}
