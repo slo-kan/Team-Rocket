82d0fd904629db65e5cc7d4fd3e9ecd5cd8ea5db
Vasudev <vasudev7412@gmail.com>
2020-05-05 18:44:38 +0200

    Added a google map view in map fragment

--- app/src/main/java/com/teamrocket/app/ui/map/MapFragment.java
+++ app/src/main/java/com/teamrocket/app/ui/map/MapFragment.java
@@ -9,15 +9,35 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.teamrocket.app.R;
 
 public class MapFragment extends Fragment {
 
     public static final String TAG = "mapFragment";
 
+    private GoogleMap map;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_map, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.mapFragment);
+        mapFragment.getMapAsync(map -> {
+            this.map = map;
+            LatLng location = new LatLng(17.385, 78.486);
+            this.map.addMarker(new MarkerOptions().position(location).title("Greenwich"));
+            this.map.moveCamera(CameraUpdateFactory.newLatLng(location));
+            this.map.animateCamera(CameraUpdateFactory.newLatLngZoom(location, 15), 2000, null);
+        });
     }
 }
