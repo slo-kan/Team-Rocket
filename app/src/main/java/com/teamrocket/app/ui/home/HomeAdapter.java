c60661fd64c601095d6719a56a592493039bc7e2
Vasudev <vasudev7412@gmail.com>
2020-05-05 12:49:43 +0200

    Added a basic recycler view to home fragment

---
+++ app/src/main/java/com/teamrocket/app/ui/home/HomeAdapter.java
@@ -0,0 +1,57 @@
+package com.teamrocket.app.ui.home;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView.Adapter;
+import androidx.recyclerview.widget.RecyclerView.ViewHolder;
+
+import com.teamrocket.app.R;
+import com.teamrocket.app.model.BirdSighting;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HomeAdapter extends Adapter<HomeAdapter.BirdSightingViewHolder> {
+
+    private ArrayList<BirdSighting> sightings = new ArrayList<>();
+
+    @NonNull
+    @Override
+    public BirdSightingViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_home_item, parent, false);
+        return new BirdSightingViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(BirdSightingViewHolder holder, int position) {
+        holder.bind(sightings.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return sightings.size();
+    }
+
+    public void update(List<BirdSighting> sightings) {
+        this.sightings.clear();
+        this.sightings.addAll(sightings);
+        notifyDataSetChanged();
+    }
+
+    static class BirdSightingViewHolder extends ViewHolder {
+        private TextView textTitle;
+
+        BirdSightingViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textTitle = itemView.findViewById(R.id.textTitleItemSighting);
+        }
+
+        void bind(BirdSighting birdSighting) {
+            textTitle.setText(birdSighting.getBird().getName());
+        }
+    }
+}
--- app/src/main/java/com/teamrocket/app/ui/home/HomeFragment.java
+++ app/src/main/java/com/teamrocket/app/ui/home/HomeFragment.java
@@ -4,21 +4,75 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
+import com.teamrocket.app.BTApplication;
 import com.teamrocket.app.R;
+import com.teamrocket.app.data.db.BirdSightingDao;
+import com.teamrocket.app.model.Bird;
+import com.teamrocket.app.model.BirdSighting;
+
+import java.util.Random;
 
 public class HomeFragment extends Fragment {
 
     public static final String TAG = "homeFragment";
 
+    private BirdSightingDao dao;
+    private HomeAdapter adapter;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        dao = ((BTApplication) getActivity().getApplication()).getBirdSightingDao();
+
+        ((AppCompatActivity) getActivity()).setSupportActionBar(view.findViewById(R.id.toolbarHome));
+
+        adapter = new HomeAdapter();
+
+        ExtendedFloatingActionButton btnAddSighting = view.findViewById(R.id.btnAddSighting);
+        btnAddSighting.setOnClickListener(v -> addBirdSighting());
+
+        RecyclerView recyclerView = view.findViewById(R.id.recyclerHome);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(adapter);
+
+        adapter.update(dao.getAll());
+    }
+
+    private void addBirdSighting() {
+        Bird bird = new Bird();
+
+        Random random = new Random();
+
+        bird.setName("Bird #" + random.nextInt(1000));
+        bird.setImagePath("");
+        bird.setFamily("Family #" + random.nextInt(25));
+        bird.setColor("violet");
+        bird.setSize(random.nextInt(3));
+
+        BirdSighting.Location location = new BirdSighting.Location(0, 0);
+        BirdSighting sighting = new BirdSighting(bird, location, System.currentTimeMillis());
+
+        long before = System.currentTimeMillis();
+        dao.insert(sighting);
+        long after = System.currentTimeMillis();
+
+        Toast.makeText(getContext(), "Saving a bird sighting took " + (after - before) + " ms", Toast.LENGTH_SHORT).show();
     }
 
     @Override
