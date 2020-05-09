a94fe8385ab9e3641b91b06820df6dd0683db4fd
Vasudev <vasudev7412@gmail.com>
2020-05-09 23:45:27 +0200

    Added a layout for add sighting activity

--- app/src/main/java/com/teamrocket/app/ui/add/AddSightingActivity.java
+++ app/src/main/java/com/teamrocket/app/ui/add/AddSightingActivity.java
@@ -1,6 +1,17 @@
 package com.teamrocket.app.ui.add;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.teamrocket.app.R;
+
 public class AddSightingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+    }
 }
