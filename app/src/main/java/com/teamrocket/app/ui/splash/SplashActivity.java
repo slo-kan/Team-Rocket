2704ddd6302f968f0ab256e526b695d2889b997f
sreepradhdingari <sreepradh.dingari@st.ovgu.de>
2020-06-13 18:07:54 +0530

    Adding splash screen to the app.

---
+++ app/src/main/java/com/teamrocket/app/ui/splash/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.teamrocket.app.ui.splash;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.teamrocket.app.R;
+import com.teamrocket.app.ui.main.MainActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    public static int SPLASH_TIME_OUT = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(() -> {
+            Intent homeIntent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(homeIntent);
+            finish();
+        }, SPLASH_TIME_OUT);
+    }
+}
